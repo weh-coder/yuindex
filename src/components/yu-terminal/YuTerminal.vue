b02f0a0bca50db0ef1be6189e655b0e5bd2b6f72
weh-coder <97448322+weh-coder@users.noreply.github.com>
2022-08-24 01:08:03 +0800

    Update YuTerminal.vue

--- src/components/yu-terminal/YuTerminal.vue
+++ src/components/yu-terminal/YuTerminal.vue
@@ -406,11 +406,7 @@ onMounted(() => {
   } else {
     terminal.writeTextOutput(
       `Welcome to YuIndex, coolest browser index for geeks!` +
-        `<a href="//github.com/liyupi/yuindex" target='_blank'> GitHub Open Source</a>`
-    );
-    terminal.writeTextOutput(
-      `Author <a href="//docs.qq.com/doc/DUFFRVWladXVjeUxW" target="_blank">coder_yupi</a>` +
-        `: please input 'help' to enjoy`
+        `<a href="//github.com/weh-coder/yuindex" target='_blank'> GitHub Open Source</a>`
     );
     terminal.writeTextOutput("<br/>");
   }
